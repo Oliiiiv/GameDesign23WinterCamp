e3a13f0f0e019dba72f7ae1a4d31abce7b1ed840
Oliiiv <weng.zhang0708@gmail.com>
2023-01-06 13:23:32 -0500

    asmt updated

--- asmt1/CodingChallenge1_2.js
+++ asmt1/CodingChallenge1_2.js
@@ -1,22 +1,13 @@
 //////////////////////////////////////////
 //Coding Challenge #1.2
 /*
-There are two gymnastics teams, Dolphins and Koalas. They compete against each
-other 3 times. The winner with the highest average score wins a trophy!
+Use the BMI example from Challenge 1_1, and the code you already wrote, and
+improve it.
 Your tasks:
-1. Calculate the average score for each team, using the test data below
-2. Compare the team's average scores to determine the winner of the competition,
-and print it to the console. Don't forget that there can be a draw, so test for that
-as well (draw means they have the same average score)
-3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
-team only wins if it has a higher score than the other team, and the same time a
-score of at least 100 points. Hint: Use a logical operator to test for minimum
-score, as well as multiple else-if blocks 😉
-4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
-both teams have the same score and both have a score greater or equal 100
-points. Otherwise, no team wins the trophy
-Test data:
-§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
-GOOD LUCK :-)*/
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 
+GOOD LUCK :-)
+*/
--- asmt2/CodingChallenge2_2.js
+++ asmt2/CodingChallenge2_2.js
@@ -1,28 +1,18 @@
 //////////////////////////////////////////
-//Coding Challenge #2.2
+//Coding Challenge #2.3
 /*
-Let's improve Steven's tip calculator even more, this time using loops!
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
 Your tasks:
-1. Create an array 'bills' containing all 10 test bill values
-2. Create empty arrays for the tips and the totals ('tips' and 'totals')
-3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
-tips and total values (bill + tip) for every bill value in the bills array. Use a for
-loop to perform the 10 calculations!
-Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
-Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
-tips and totals arrays 
-Bonus:
-4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
-an argument. This function calculates the average of all numbers in the given
-array. This is a difficult challenge (we haven't done this before)! Here is how to
-solve it:
-4.1. First, you will need to add up all values in the array. To do the addition,
-start by creating a variable 'sum' that starts at 0. Then loop over the
-array using a for loop. In each iteration, add the current value to the
-'sum' variable. This way, by the end of the loop, you have all values
-added together
-4.2. To calculate the average, divide the sum you calculated before by the
-length of the array (because that's the number of elements)
-4.3. Call the function with the 'totals' array
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall.
 GOOD LUCK :-)
  */
